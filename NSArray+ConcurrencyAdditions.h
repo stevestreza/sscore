94389cd690f8ad01c5f886d6d2e2f79b6f745d8e
Steve Streza <stevestreza@gmail.com>
2009-09-13 18:55:53 -0400

    Added documentation and cleaned up source

--- NSArray+ConcurrencyAdditions.h
+++ NSArray+ConcurrencyAdditions.h
@@ -1,27 +1,74 @@
-//
-//  NSArray+ConcurrencyAdditions.h
-//  ConcurrencyExtensions
-//
-//  Created by Steve Streza on 9/13/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/* 
+
+ NSArray+ConcurrencyAdditions.h
+ 
+ Copyright (c) 2009 Steve Streza
+ Licensed under the MIT License
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ */
 
 #import <Foundation/Foundation.h>
 
+/*
+ Block typedef for creating an object at a given index.
+ You should return an autoreleased object in your populator.
+ To use:
+ 
+ SSPopulator populator = ^SSPopulator(NSUInteger number){
+	return [NSNumber numberWithInt:(2*number + 1)];
+ }
+ */
 typedef NSObject* (^SSPopulator)(NSUInteger index);
 
+/*
+ These methods create or modify an existing array using a
+ populator block. Supply NSEnumerationConcurrent for options
+ to use Grand Central Dispatch to create objects concurrently,
+ or supply 0 to create objects serially.
+ */
 @interface NSArray (ConcurrencyAdditions)
 
-+(NSArray *)SS_arrayWithCount:(NSUInteger)count withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator;
++(NSArray *)SS_arrayWithCount:(NSUInteger)count
+				  withOptions:(NSEnumerationOptions)options
+					populator:(SSPopulator)populator;
 
--(id)SS_arrayByAddingCount:(NSUInteger)count withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator;
--(id)SS_arrayByInsertingCount:(NSUInteger)count atIndex:(NSUInteger)index withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator;
+-(id)SS_arrayByAddingCount:(NSUInteger)count 
+			   withOptions:(NSEnumerationOptions)options 
+				 populator:(SSPopulator)populator;
 
+-(id)SS_arrayByInsertingCount:(NSUInteger)count 
+					  atIndex:(NSUInteger)index 
+				  withOptions:(NSEnumerationOptions)options 
+					populator:(SSPopulator)populator;
 @end
 
 @interface NSMutableArray (ConcurrencyAdditions)
 
--(void)SS_addCount:(NSUInteger)count withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator;
--(void)SS_insertCount:(NSUInteger)count atIndex:(NSUInteger)index withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator;
+-(void)SS_addCount:(NSUInteger)count
+	   withOptions:(NSEnumerationOptions)options 
+		 populator:(SSPopulator)populator;
+
+-(void)SS_insertCount:(NSUInteger)count
+			  atIndex:(NSUInteger)index
+		  withOptions:(NSEnumerationOptions)options 
+			populator:(SSPopulator)populator;
 
 @end
--- NSArray+ConcurrencyAdditions.m
+++ NSArray+ConcurrencyAdditions.m
@@ -1,19 +1,43 @@
-//
-//  NSArray+ConcurrencyAdditions.m
-//  ConcurrencyExtensions
-//
-//  Created by Steve Streza on 9/13/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/* 
+
+ NSArray+ConcurrencyAdditions.m
+ 
+ Copyright (c) 2009 Steve Streza
+ Licensed under the MIT License
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ */
 
 #import "NSArray+ConcurrencyAdditions.h"
 #import <dispatch/dispatch.h>
 
 @implementation NSArray (ConcurrencyAdditions)
 
-+(NSArray *)SS_arrayWithCount:(NSUInteger)count withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator{
++(NSArray *)SS_arrayWithCount:(NSUInteger)count 
+				  withOptions:(NSEnumerationOptions)options
+					populator:(SSPopulator)populator{
+	
 	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+
 	dispatch_queue_t dispatchQueue = NULL;
+
 	if(options != NSEnumerationConcurrent){
 //		NSLog(@"Using serial queue");
 		dispatchQueue = dispatch_queue_create("com.stevestreza.nsarray.serial", 0);
@@ -25,57 +49,71 @@
 	
 	if(!dispatchQueue) return nil;
 	
-	NSPointerArray *pointers = [NSPointerArray pointerArrayWithWeakObjects];
-	[pointers setCount:count];
-	
 	NSObject **objectArray = calloc(sizeof(NSObject *),count);
 	
-	__block NSUInteger ticks = 0;
-	
-//	NSLog(@"Adding operations");
 	dispatch_apply((size_t)count, dispatchQueue, ^(size_t index){
 		NSObject *object = populator(index);
-//		NSLog(@"Inserting object at index: %i - %@", index, object);
-		objectArray[index] = [object retain];
+		objectArray[index] = [object retain]; // we release the object again at the end of the method
 	});
-//	NSLog(@"Done adding operations");
 	
 	dispatch_release(dispatchQueue);
+	dispatchQueue = NULL;
 	
-//	NSLog(@"1) Retain count %i, ",[objectArray[5] retainCount]);
-	NSArray *retArray = [[NSArray arrayWithObjects:objectArray count:count] retain];
-//	NSLog(@"2) Retain count %i, ",[[retArray objectAtIndex:5] retainCount]);
+	NSArray *retArray = [[self arrayWithObjects:objectArray count:count] retain];
 	
 	[pool drain];
 	free(objectArray);
 
 	[retArray autorelease];
 
-//	NSLog(@"3) Retain count %i, %i",[[retArray objectAtIndex:5] retainCount], [retArray count]);
 	[retArray makeObjectsPerformSelector:@selector(release)];
-
 	return retArray;
 }
 
--(id)SS_arrayByAddingCount:(NSUInteger)count withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator{
-	NSArray *newArray = [NSArray SS_arrayWithCount:count withOptions:options populator:populator];
+-(id)SS_arrayByAddingCount:(NSUInteger)count
+			   withOptions:(NSEnumerationOptions)options 
+				 populator:(SSPopulator)populator{
+	
+	NSArray *newArray = [NSArray SS_arrayWithCount:count
+									   withOptions:options
+										 populator:populator];
 	return [self arrayByAddingObjectsFromArray:newArray];
 }
 
--(id)SS_arrayByInsertingCount:(NSUInteger)count atIndex:(NSUInteger)index withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator{
-	return nil;
+-(id)SS_arrayByInsertingCount:(NSUInteger)count
+					  atIndex:(NSUInteger)index
+				  withOptions:(NSEnumerationOptions)options
+					populator:(SSPopulator)populator{
+	
+	NSMutableArray *array = [[self mutableCopy] autorelease];
+	[array SS_insertCount:count
+				  atIndex:index
+			  withOptions:options
+				populator:populator];
+	return array;
 }
 
 @end
 
 @implementation NSMutableArray (ConcurrencyAdditions)
 
--(void)SS_addCount:(NSUInteger)count withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator{
-	[self addObjectsFromArray:[NSArray SS_arrayWithCount:count withOptions:options populator:populator]];
+-(void)SS_addCount:(NSUInteger)count
+	   withOptions:(NSEnumerationOptions)options
+		 populator:(SSPopulator)populator{
+	
+	[self addObjectsFromArray:[NSArray SS_arrayWithCount:count 
+											 withOptions:options
+											   populator:populator]];
 }
 
--(void)SS_insertCount:(NSUInteger)count atIndex:(NSUInteger)index withOptions:(NSEnumerationOptions)options populator:(SSPopulator)populator{
-	[self insertObjects:[NSArray SS_arrayWithCount:count withOptions:options populator:populator]
+-(void)SS_insertCount:(NSUInteger)count
+			  atIndex:(NSUInteger)index
+		  withOptions:(NSEnumerationOptions)options
+			populator:(SSPopulator)populator{
+	
+	[self insertObjects:[NSArray SS_arrayWithCount:count 
+									   withOptions:options
+										 populator:populator]
 			  atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(index, index+count)]];
 }
 
